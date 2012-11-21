0294108c9f8d48d05c7619d2b15e70dd88f13a45
hyq1860 <pchero@163.com>
2012-11-21 12:25:52 +0800

    测试

--- DotNet.IoC/ServiceLocator/UnityServiceLocator.cs
+++ DotNet.IoC/ServiceLocator/UnityServiceLocator.cs
@@ -12,7 +12,7 @@ namespace DotNet.IoC.ServiceLocator
     using System.Collections.Generic;
     using System.Linq;
     using System.Text; 
-    // 测试
+    // 测试 
     //public class UnityServiceLocator : ServiceLocatorImplBase
     //{
     //    private IUnityContainer _container;

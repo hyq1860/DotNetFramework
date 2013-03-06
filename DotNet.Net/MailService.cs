203a28bd520e4fa29e49b8f6fee27b1ee2b6ce07
hyq1860 <pchero@163.com>
2013-03-07 01:13:45 +0800

    提交

---
+++ DotNet.Net/MailService.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MailService.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace DotNet.Net
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// 发送邮件
+    /// </summary>
+    public class MailService
+    {
+    }
+}

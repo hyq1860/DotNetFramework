f96911dddf85530a5ba12112080124e244ff42df
hyq1860 <pchero@163.com>
2013-09-15 18:00:35 +0800

    js代码及mainui.cshtml

---
+++ DotNet.MVC.WebAllInOne/Scripts/Common.js
@@ -0,0 +1,35 @@
+﻿//原生JavaScript实现字符串长度截取
+function cutstr(str, len) {
+    var temp;
+    var icount = 0;
+    var patrn = /[^\x00-\xff]/;
+    var strre = "";
+    for (var i = 0; i < str.length; i++) {
+        if (icount < len - 1) {
+            temp = str.substr(i, 1);
+            if (patrn.exec(temp) == null) {
+                icount = icount + 1;
+            } else {
+                icount = icount + 2;
+            }
+            strre += temp;
+        } else {
+            break;
+        }
+    }
+    return strre + "...";
+}
+
+//原生JavaScript获取域名主机
+function getHost(url) {
+    var host = "null";
+    if (typeof url == "undefined" || null == url) {
+        url = window.location.href;
+    }
+    var regex = /^\w+\:\/\/([^\/]*).*/;
+    var match = url.match(regex);
+    if (typeof match != "undefined" && null != match) {
+        host = match[1];
+    }
+    return host;
+}
